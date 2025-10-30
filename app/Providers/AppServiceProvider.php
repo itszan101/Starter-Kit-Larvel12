81f4a5d0c21f5452097656f1c806f6ab5e6f896a
itszan101 <itszan29@gmail.com>
2025-10-30 12:25:36 +0700

    configVercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        // Force HTTPS in production
+        if (config('app.url') == 'https://link-to-your-app.vercel.app') {
+            \URL::forceScheme('https');
+        }
     }
 }
